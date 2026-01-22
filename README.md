f8f4450f58d5757326fb3c6f1ef967d1f1903d34
Aji Respati <aji@voltras.co.id>
2026-01-22 15:34:46 +0700

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# umkm_frontend
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/umkm_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.umkm_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
